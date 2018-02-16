b4ff0680ecea87bbf13ae38ee01f35a436353d97
lapots <sebastrident@gmail.com>
2018-02-16 16:04:36 +0300

    Sample Spring Boot application

---
+++ src/main/java/com/lapots/breed/judge/JudgeRuleEngineApplication.java
@@ -0,0 +1,12 @@
+package com.lapots.breed.judge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudgeRuleEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JudgeRuleEngineApplication.class, args);
+    }
+}
---
+++ src/test/java/com/lapots/breed/judge/JudgeRuleEngineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lapots.breed.judge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JudgeRuleEngineApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
