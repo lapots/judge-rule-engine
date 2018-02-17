457332c456ce2141db7f39572eed058d98338cdb
lapots <sebastrident@gmail.com>
2018-02-17 13:46:14 +0300

    Basic router function

--- src/main/java/com/lapots/breed/judge/WebConfig.java
+++ src/main/java/com/lapots/breed/judge/WebConfig.java
@@ -1,7 +1,22 @@
 package com.lapots.breed.judge;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.BodyInserters.fromPublisher;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class WebConfig {
+
+    @Bean
+    public RouterFunction<?> helloRoute() {
+        return route(GET("/judge/router/hello"),
+                request -> ServerResponse.ok().body(fromPublisher(Mono.just("Hello Router WebFlux"), String.class)));
+    }
+
 }
