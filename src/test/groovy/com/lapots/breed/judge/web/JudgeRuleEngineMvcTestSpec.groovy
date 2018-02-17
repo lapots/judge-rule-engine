f327d82a089489445db7cd6904a1f497074233a4
lapots <sebastrident@gmail.com>
2018-02-17 21:20:21 +0300

    Workaround for router function tests

--- src/test/groovy/com/lapots/breed/judge/web/JudgeRuleEngineMvcTestSpec.groovy
+++ src/test/groovy/com/lapots/breed/judge/web/JudgeRuleEngineMvcTestSpec.groovy
@@ -3,7 +3,6 @@ package com.lapots.breed.judge.web
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.test.web.reactive.server.WebTestClient
-import spock.lang.Ignore
 import spock.lang.Specification
 
 @WebFluxTest
@@ -21,13 +20,4 @@ class JudgeRuleEngineMvcTestSpec extends Specification {
                     .isEqualTo("Hello WebFlux")
     }
 
-    @Ignore
-    def "router function returns hello"() {
-        expect:
-            webClient.get().uri("/judge/router/hello")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(String.class)
-                .isEqualTo("Hello WebFlux") // should fail
-    }
 }
---
+++ src/test/groovy/com/lapots/breed/judge/web/JudgeRuleEngineReactiveWebTestSpec.groovy
@@ -0,0 +1,39 @@
+package com.lapots.breed.judge.web
+
+import com.lapots.breed.judge.WebConfig
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.context.ApplicationContext
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.web.reactive.server.WebTestClient
+import spock.lang.Specification;
+
+/**
+ * https://github.com/spring-projects/spring-boot/issues/10683
+ */
+@ContextConfiguration(classes = [ WebConfig.class ])
+@WebFluxTest
+class JudgeRuleEngineReactiveWebTestSpec extends Specification {
+
+    @Autowired
+    ApplicationContext context;
+
+    WebTestClient webClient
+
+    /*
+        https://github.com/spockframework/spock/issues/76
+     */
+    def setup() {
+        webClient = WebTestClient.bindToApplicationContext(context).build()
+    }
+
+    def "router function returns hello"() {
+        expect:
+            webClient.get().uri("/judge/router/hello")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class)
+                .isEqualTo("Hello Router WebFlux")
+    }
+
+}
