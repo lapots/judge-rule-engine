cd7f7adaadab553ad0066d607f05f28617048ea4
lapots <sebastrident@gmail.com>
2018-02-18 16:51:26 +0300

    Minor refactoring is shell script

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,9 @@
 set -o errexit -o nounset
 
 terraform init
-terraform plan
 
+terraform plan -destroy
 terraform destroy -target heroku_app.judge_re -force
 
+terraform plan
 terraform apply -auto-approve
