57a87d34b4d878070be1b2ada61321dab27a714c
lapots <sebastrident@gmail.com>
2018-04-17 16:13:16 +0300

    Added initialization with pre-backed xml rule

--- src/main/java/com/lapots/breed/judge/WebConfig.java
+++ src/main/java/com/lapots/breed/judge/WebConfig.java
@@ -2,15 +2,23 @@ package com.lapots.breed.judge;
 
 import com.deliveredtechnologies.rulebook.lang.RuleBookBuilder;
 import com.deliveredtechnologies.rulebook.model.RuleBook;
+import com.deliveredtechnologies.rulebook.model.runner.RuleAdapter;
 import com.lapots.breed.judge.domain.Player;
 import com.lapots.breed.judge.domain.PlayerLevel;
 import com.lapots.breed.judge.repository.PlayerLevelRepository;
 import com.lapots.breed.judge.repository.PlayerRepository;
+import com.lapots.breed.rule.builder.RuleClassGenerator;
+import com.lapots.breed.rule.compiler.OpenhftCachedCompiler;
+import com.lapots.breed.rule.generator.wrapper.ClassGeneratorWrapper;
+import com.lapots.breed.rule.parser.DefaultRuleParser;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.io.InvalidClassException;
+import java.util.List;
 
 /**
  * Default initializations.
@@ -48,5 +56,36 @@ public class WebConfig {
     @Bean
     public RuleBook ruleBook() {
         return RuleBookBuilder.create().build();
+    }
+
+    /**
+     * Instantiates rule engine with simple rule.
+     * @param ruleBook rule book
+     * @return cmd runner
+     */
+    @Bean
+    public CommandLineRunner initRuleEngine(final RuleBook ruleBook) {
+        return args -> {
+            List<Class<?>> rules = new RuleClassGenerator()
+                    .withBuilderType(RuleClassGenerator.BuilderType.FILE)
+                    .withClassLoader(this.getClass().getClassLoader())
+                    .withCompiler(new OpenhftCachedCompiler())
+                    .withGenerator(new ClassGeneratorWrapper())
+                    .withParser(new DefaultRuleParser())
+                    .generate("level_up_rule.xml");
+            rules.stream().map(ruleClass -> {
+                try {
+                    return ruleClass.newInstance();
+                } catch (InstantiationException | IllegalAccessException e) {
+                    throw new IllegalStateException("Failed to process parsed rule!", e);
+                }
+            }).map(rule -> {
+                try {
+                    return new RuleAdapter(rule);
+                } catch (InvalidClassException e) {
+                    throw new IllegalStateException("Failed to create rule adapter!", e);
+                }
+            }).forEach(ruleBook::addRule);
+        };
     }
 }
